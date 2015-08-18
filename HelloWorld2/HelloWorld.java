cf486def7de22027684063ed8124a3a80e5df788
talit <tpanikashvili@gmail.com>
2015-08-18 09:20:34 -0700

    Hello World

---
+++ HelloWorld2/HelloWorld.java
@@ -0,0 +1,17 @@
+
+/**
+ * This will do a simple printIn. Our first java code! 
+ * 
+ * @author Talita Panikashvili 
+ * @version August 18, 2015
+ */
+public class HelloWorld
+{
+ 
+   public static void main (String[] args)
+{
+    System.out.println ("Hello World! From Talita.");
+}
+    
+
+}
